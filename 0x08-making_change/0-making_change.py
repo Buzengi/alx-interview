76dcbb76a2b157dd10ea4a3b686fd575f83e0c9b
Chidi <ajaerohc@gmail.com>
2023-08-25 00:34:01 +0100

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,19 +13,17 @@ def makeChange(coins, total):
     Module tries to give exact change given a list of coins with a 
     specified values if unable to do so returns -1
     """
-
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
-
-    if table[total] == sys.maxsize:
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
         return -1
-    return table[total]
+    return change
